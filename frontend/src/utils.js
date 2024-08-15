21923e4a9002f8fce755a36fb1fb2102eebca53e
Arthur Papin <arthurpapin49@gmail.com>
2024-08-15 18:27:41 +0200

    Initial commit

---
+++ frontend/src/utils.js
@@ -0,0 +1,8 @@
+// Fonction pour obtenir le token CSRF
+export function getCSRFToken() {
+  const name = 'csrftoken';
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
